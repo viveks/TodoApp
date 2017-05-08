66953ff9884faae43527b5e15ab137b857696477
Vivek Sinha <viveks@myparichay.in>
2017-05-07 20:36:39 -0400

    Update test

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('590d343a6c74b37352f02549')
+  // },{
+  //   $set: {
+  //     completed: true,
+  //     text: 'walk the dog is done'
+  //   }
+  // },
+  // {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // }, (err)=> {
+  //   console.log(err);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    name: 'Vivek'
+  },{
+    $set: {
+      name: 'Mayura'
+    },
+    $inc: {
+      age: -1
+    }
+  },{
+    returnOriginal: false
+  }).then((result)=> {
+    console.log(result);
+  });
+
+  // db.close();
+});
