edae4ebd57b11bb1819bb3d1f44a28218e85745f
Vivek Sinha <viveks@myparichay.in>
2017-05-09 23:05:33 -0400

    New post route

---
+++ server/models/Todo.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+module.exports = {Todo};
--- server/server.js
+++ server/server.js
@@ -1,52 +1,26 @@
-const mongoose = require ('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require ('./db/db');
+var {Todo} = require ('./db/Todo');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=> {
+  var todo = new Todo({
+    text: req.body.text
+  }) ;
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  }, (e)=>{
+    console.log(e);
+    res.status(400).send(e);
+  });
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-//
-// newTodo.save().then((doc)=>{
-//   console.log(doc);
-// }, (e)=>{
-//   console.log(e);
-// });
-
-
-// newTodo.save().then((doc)=>{
-//   console.log(doc);
-// }, (e)=>{
-//   console.log(e);
-// });
-
-var User = mongoose.model('User',{
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 1
-  }
-});
-
-var newUser = new User({
-  email: 'vinisinh@gmail.com '
-});
-
-newUser.save().then((doc)=>{
-  console.log(doc)
-},(e)=>{
-  console.log(e);
+app.listen(3000,() => {
+  console.log('started on port 3000');
 });
